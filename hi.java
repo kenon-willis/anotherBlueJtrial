d9d54985c4106c6bf8881d90836a906b1db8a6cc
Kenon Willis <kwillis@lvjusd.k12.ca.us>
2018-09-20 10:22:27 -0700

    added 2nd project

---
+++ hi.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class hi here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class hi
+{
+    public static void main(){
+        System.out.println("hi");
+    }
+}
